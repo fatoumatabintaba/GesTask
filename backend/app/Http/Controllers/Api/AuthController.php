9ccec21327c1b5b3132883780bb620ab60e54979
fatoumatabinta ba <bintadjenga1@gmail.com>
2025-08-06 15:31:55 +0200

    authC

--- backend/app/Http/Controllers/Api/AuthController.php
+++ backend/app/Http/Controllers/Api/AuthController.php
@@ -49,10 +49,9 @@ class AuthController extends Controller
         $token = $user->createToken('auth_token')->plainTextToken;
 
        return response()->json([
-        'access_token' => $token,
-        'token_type' => 'Bearer',
-        'user' => $user
-]);
+            'token' => $token,
+            'user' => $user
+        ]);
 
     }
 
